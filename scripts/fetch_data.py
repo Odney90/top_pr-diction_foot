2d9e43efd87e6b0043d0da5cde2c1ea9f7e98edc
Odney90 <78322756+Odney90@users.noreply.github.com>
2025-03-01 15:12:26 +0100

    Update fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -3,20 +3,25 @@ import pandas as pd
 
 def fetch_data():  
     url = "https://api.soccersapi.com/v2.2/leagues/?user=lundiodney&token=623654d91c81ceed9379be5968f089d8&t=list"  
+    
+    # Effectuer la requête GET  
     response = requests.get(url)  
     
+    # Vérifier si la requête a réussi  
     if response.status_code == 200:  
-        data = response.json()  
-        leagues = data['data']  
-        return leagues  
+        data = response.json()  # Convertir la réponse en JSON  
+        
+        # Extraire les informations nécessaires  
+        leagues = data.get('leagues', [])  
+        
+        # Créer un DataFrame à partir des données  
+        df = pd.DataFrame(leagues)  
+        
+        # Enregistrer le DataFrame dans un fichier CSV  
+        df.to_csv('data/matchs.csv', index=False)  
+        print("Données récupérées et enregistrées dans matchs.csv")  
     else:  
-        print("Erreur lors de la récupération des données :", response.status_code)  
-        return None  
+        print(f"Erreur lors de la récupération des données : {response.status_code}")  
 
-if __name__ == "__main__":  
-    leagues_data = fetch_data()  
-    if leagues_data:  
-        # Convertir les données en DataFrame et les sauvegarder dans un fichier CSV  
-        leagues_df = pd.DataFrame(leagues_data)  
-        leagues_df.to_csv('data/leagues.csv', index=False)  
-        print("Données des ligues sauvegardées dans data/leagues.csv")  
+# Appeler la fonction pour récupérer les données  
+fetch_data()  
