5527f1aa57710734dfd40326986221eac627d422
Odney90 <78322756+Odney90@users.noreply.github.com>
2025-03-01 14:41:01 +0100

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,18 +1,22 @@
 import requests  
 import pandas as pd  
 
-def fetch_data(api_url):  
-    response = requests.get(api_url)  
+def fetch_data():  
+    url = "https://api.soccersapi.com/v2.2/leagues/?user=lundiodney&token=623654d91c81ceed9379be5968f089d8&t=list"  
+    response = requests.get(url)  
+    
     if response.status_code == 200:  
         data = response.json()  
-        # Supposons que les données soient sous forme de liste de dictionnaires  
-        leagues = data.get('response', [])  # Accéder à la clé 'response' pour obtenir les données  
-        df = pd.DataFrame(leagues)  
-        df.to_csv('data/matchs.csv', index=False)  
-        print("Données récupérées et sauvegardées dans data/matchs.csv")  
+        leagues = data['data']  
+        return leagues  
     else:  
         print("Erreur lors de la récupération des données :", response.status_code)  
+        return None  
 
 if __name__ == "__main__":  
-    api_url = "https://api.soccersapi.com/v2.2/leagues/?user=lundiodney&token=623654d91c81ceed9379be5968f089d8&t=list"  
-    fetch_data(api_url)  
+    leagues_data = fetch_data()  
+    if leagues_data:  
+        # Convertir les données en DataFrame et les sauvegarder dans un fichier CSV  
+        leagues_df = pd.DataFrame(leagues_data)  
+        leagues_df.to_csv('data/leagues.csv', index=False)  
+        print("Données des ligues sauvegardées dans data/leagues.csv")  
